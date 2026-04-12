d94d4ad25cb9af92fc2f18129e09f36bbb16f4ee
olilla92 <c0ocslil@vasvari.org>
2026-04-12 18:52:51 +0200

    Add project files.

---
+++ Pontkalkulator/Models/StudentCredit.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pontkalkulator.Models
+{
+    public class StudentCredit
+    {
+        public string _studentName;
+        private string _neptunCode;
+        private int _creditNumber;
+
+        public StudentCredit() { }
+        public StudentCredit(string studentName, string neptunCode, int creditNumber)
+        {
+            _studentName = studentName;
+            _neptunCode = neptunCode;
+            _creditNumber = creditNumber;
+        }
+
+        public string StudentName
+        {
+            get => _studentName;
+            set => _studentName = value;
+        }
+        public string NeptunCode
+        {
+            get => _neptunCode;
+            set => _neptunCode = value;
+        }
+        public int CreditNumber
+        {
+            get => _creditNumber;
+            set => _creditNumber = value;
+        }
+
+        public void SetName (string newName)
+        {
+            if (newName == _studentName)
+                throw new ArgumentException("Már ez a neved.");
+            else if (!newName.Contains(" "))
+                throw new ArgumentException("Nem adtál meg teljes nevet.");
+            else
+                _studentName = newName;
+        }
+
+        public void SetNeptunCode (string newCode)
+        {
+            if (string.IsNullOrEmpty(newCode)) throw new ArgumentException("Neptun kód nem lehet üres.");
+            else
+                _neptunCode = newCode;
+        }
+
+        public bool IsFinished()
+        {
+            if (_creditNumber >= 44)
+                return true;
+            else
+                return false;
+        }
+
+        public int LeftCredit()
+        {
+            if (_creditNumber < 44)
+                return 44 - _creditNumber;
+            else
+                return _creditNumber;
+        }
+
+        public int IncreaseCredit(int coursePoint)
+        {
+            if (coursePoint <= 0)
+                throw new ArgumentOutOfRangeException("A kredit csak pozitív értékkel növelhető.");
+            else
+                return _creditNumber += coursePoint;
+        }
+
+        public void StartOfSemester(bool isStart)
+        {
+            if (isStart == true)
+                _creditNumber = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{_studentName} - {_neptunCode} (kredit: {_creditNumber})";
+        }
+    }
+}
---
+++ Pontkalkulator/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Pontkalkulator.Models;
+
+Console.WriteLine("Hello, World!");
+StudentCredit first = new StudentCredit("Kis Anna", "neptunkod123", 43);
+try
+{
+    Console.WriteLine(first);
+    first.SetName("Nagy Piroska");
+    first.SetNeptunCode("ABSC1564");
+    Console.WriteLine(first);
+}catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+if(first.IsFinished() == true)
+    Console.WriteLine("Félév teljesítve.");
+else
+Console.WriteLine($"Félév nem teljesítve. {first.LeftCredit()} credit hiányzik a félév teljesítéséhez.");
+
+Console.Write("Adjon meg egy kurzus kreditértéket: ");
+int coursePoint = Convert.ToInt32(Console.ReadLine());
+first.IncreaseCredit(coursePoint);
+Console.WriteLine(first);
+
+Console.Write("Kezd új félévet? ");
+bool newSemester = Convert.ToBoolean(Console.ReadLine());
+first.StartOfSemester(newSemester);
+Console.WriteLine(first);
